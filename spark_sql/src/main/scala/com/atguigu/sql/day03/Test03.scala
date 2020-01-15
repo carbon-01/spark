05587d43358fefb6785c3f33c2ad522a0842d1b7
carbon <vrea@outlook.com>
2020-01-15 17:50:48 +0800

    commit

--- spark_sql/src/main/scala/com/atguigu/sql/day03/Test03.scala
+++ spark_sql/src/main/scala/com/atguigu/sql/day03/Test03.scala
@@ -1,5 +1,20 @@
 package com.atguigu.sql.day03
 
-object Test03 {
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.dstream.{DStream, ReceiverInputDStream}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
 
+object Test03 {
+  def main(args: Array[String]): Unit = {
+    val conf: SparkConf = new SparkConf().setMaster("local[2]").setAppName("Test03")
+    val scc = new StreamingContext(conf,Seconds(2))
+    scc.checkpoint("./ck3")
+
+    val sourceStream: ReceiverInputDStream[String] = scc.socketTextStream("hadoop103", 9999)
+    val windowStream: DStream[String] = sourceStream.window(Seconds(4), Seconds(10))
+    val resultStream: DStream[(String, Int)] = windowStream.flatMap(_.split("\\W+")).map((_, 1)).reduceByKey(_ + _)
+    resultStream.print(100)
+    scc.start()
+    scc.awaitTermination()
+  }
 }
